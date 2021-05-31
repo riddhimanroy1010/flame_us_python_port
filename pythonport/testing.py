a2c7ae49375209acd7b4869f7478eb74fd414be1
rroy <riddhiman.roy2015@gmail.com>
2021-05-31 15:57:02 -0400

    more testing

--- pythonport/testing.py
+++ pythonport/testing.py
@@ -1,5 +1,6 @@
 ''' py file for testing kek '''
 from dataclasses import dataclass, field
+from numpy import NaN, mat, newaxis
 import pandas as pd
 import openpyxl
 import re
@@ -7,6 +8,9 @@ import re
 from pandas.core.indexes.range import RangeIndex
 
 vh_techno                          = pd.read_csv("inputs/data_input_management.csv")
+
+
+'''
 conv                                = pd.read_csv(vh_techno.loc[vh_techno['Variable_name'] == 'conversion_units', 'File'].array[0])
 
 print(conv)
@@ -33,7 +37,28 @@ test = ["a", "b"]
 test_dt = pd.DataFrame(index = test, columns= RangeIndex(1, 10))
 
 print(test_dt)
+hist_mc                                 = pd.read_csv(vh_techno.loc[vh_techno['Variable_name'] == 'fleet_mt_comp_hist', 'File'].array[0])
+mat_hist_mc                             = hist_mc[(hist_mc["Technology"] == "ICEV-G") & (hist_mc["Size"] == "Car") & (hist_mc["Model_year"] >= 1975) & (hist_mc["Material"] != "Total")]
+print("mat_hist_mc")
+print(mat_hist_mc)
+new = pd.DataFrame(index=pd.unique(mat_hist_mc["Material"]), columns=pd.unique(mat_hist_mc["Model_year"]))
+print(vh_techno.loc[vh_techno["Variable_name"] == "model_matching_technology"]["File"])
+material_dt                             = pd.read_excel((vh_techno.loc[(vh_techno["Variable_name"] == "model_matching_material")]["File"]).array[0], (vh_techno.loc[(vh_techno["Variable_name"] == "model_matching_material")]["Sheet_name"]).array[0], engine="openpyxl")
+print("material_dt")
+print(material_dt)
 
+material_composition               = pd.DataFrame(index=pd.unique(material_dt["Own"]), columns=RangeIndex(1975, 2015))
+print("material_composition")
+print(material_composition)
+print("new")
+print(new)
+
+for year in pd.unique(mat_hist_mc["Model_year"]):
+    for material in pd.unique(mat_hist_mc["Material"]):
+        new[year][material] = (mat_hist_mc.loc[(mat_hist_mc["Model_year"] == year) & (mat_hist_mc["Material"] == material)]["Value"]).array[0]
+
+print(new)
+    
 
 '''
 
@@ -53,8 +78,15 @@ for col in tmp_mat_hist_fc.columns:
             
 
 print(tmp_mat_hist_fc)
-tmp_mat_hist_fc = tmp_mat_hist_fc * 2
-'''
+
+if pd.isnull(tmp_mat_hist_fc).values.any():
+    na_cols = [cols for cols in tmp_mat_hist_fc.columns if pd.isna(tmp_mat_hist_fc).values.any() if re.findall('[0-9]+', cols) != [] if int(re.findall('[0-9]+', cols)[0]) > 1999]
+    print(na_cols)
+
+df = pd.DataFrame([['a.1','b.1','c.1'],['a.2','b.2','c.2'],['a.3','b.3','c.3']],columns=['A','B','C'])
+
+df['A'] = df['B']
+print(df)
 '''
 technology = 'ICEV-G'
 size = 'Car'
