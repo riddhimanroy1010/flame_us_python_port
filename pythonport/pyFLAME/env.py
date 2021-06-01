1d253968e95428bac6c75f9ca043d037ecf533e4
rroy <riddhiman.roy2015@gmail.com>
2021-06-01 10:45:33 -0400

    added shelving to make environment persistent

--- pythonport/pyFLAME/env.py
+++ pythonport/pyFLAME/env.py
@@ -2,9 +2,8 @@ from dataclasses import dataclass, field
 import pandas as pd
 import numpy as np
 
-import pyFLAME as FLAME
 @dataclass
-class env:
+class environment:
     isNone: bool = field(default = True, repr = False)
     objects: dict = field(default_factory=dict)
 
@@ -16,23 +15,6 @@ class env:
                 f_to_call = objName[:-4] + "(self)"
                 self.objects[objName] = eval(f_to_call)
 
-        
-def res_env_blank_check(env):
-    if env.isNone == True:
-        return True
-    return False
-
-def res_env_new_attr(env):
-    if not env.isNone:
-        env.objects["new"] = "happy"
-        print(env.objects["new"])
-
-def res_env_new_dt(env):
-    if not env.isNone:
-        env.objects["elec_source_ev_f_res"] = pd.DataFrame(np.array([1, 2, 3]), columns=["col1"])
-
-def elec_source_ev_f(env):
-    return pd.DataFrame(np.array([1, 2, 3]), columns=["col1"])
 
 
 
