7fe5d6aa410ccfed84f45d2d30ecf3ea46c0770a
rroy <riddhiman.roy2015@gmail.com>
2021-06-04 16:26:46 -0400

    more formatting change

--- pythonport/pyFLAME/fleetClass.py
+++ pythonport/pyFLAME/fleetClass.py
@@ -57,7 +57,7 @@ class fleetClass():
                     try:
                         value                   = (hist_stock_dt.loc[(hist_stock_dt["Year"] == year) & (hist_stock_dt["Technology"] == tech) & (hist_stock_dt["Size"] == size)]["Value"]).array[0]
                         index                   = (dataframe_stock_hist[(dataframe_stock_hist['Size'] == size) & (dataframe_stock_hist['Technology'] == tech)].index)
-                        dataframe_stock_hist_temp.at[index, year] \
+                        dataframe_stock_hist.at[index, year] \
                                                 = value
                     except IndexError:
                         pass
@@ -77,42 +77,44 @@ class fleetClass():
                     try:
                         value                   = (hist_sales_dt.loc[(hist_sales_dt["Year"] == year) & (hist_sales_dt["Technology"] == tech) & (hist_sales_dt["Size"] == size)]["Value"]).array[0]
                         index                   = (dataframe_sales_hist[(dataframe_sales_hist['Size'] == size) & (dataframe_sales_hist['Technology'] == tech)].index)
-                        dataframe_sales_hist_temp.at[index, year] \
+                        dataframe_sales_hist.at[index, year] \
                                                 = value
                     except IndexError:
                         pass
 
+        self.ldv_on_road_stock                  = dataframe_stock_hist
+        self.ldv_sales                          = dataframe_sales_hist
 
 
     '''
     Returns the fields of the class as a pandas.core.frame.DataFrame
     '''
     def get_data_frame(self, field_name):
-        out = pd.DataFrame(vars(self)[field_name])
+        out                                     = pd.DataFrame(vars(self)[field_name])
         if type(vars(self)[field_name]) == "numpy.matrix":
-            temp = pd.DataFrame(vars(self)[field_name])
+            temp                                = pd.DataFrame(vars(self)[field_name])
             ##same problem: what does the cbind line do?
             temp.concat(temp, self.technology, self.size)
             out.concat(temp[["Year", "Value"]])
             for row, data in out.iterrows():
-                str = out['Type'][row]
-                val = float(re.findall('_', out['Type'][row]))
-                out['Size'][row] = str[:val - 1]
-                out['Technology'][row] = str[:val + 1200]
-                out['Type'][row] = None
+                str                             = out['Type'][row]
+                val                             = float(re.findall('_', out['Type'][row]))
+                out['Size'][row]                = str[:val - 1]
+                out['Technology'][row]          = str[:val + 1200]
+                out['Type'][row]                = None
         elif type(vars(self)[field_name]) == list:
             for i in range(len(vars(self)[field_name])):
-                tmp_stock_dt = pd.DataFrame(vars(self)[field_name][i])
+                tmp_stock_dt                    = pd.DataFrame(vars(self)[field_name][i])
                 tmp_stock_dt.concat(tmp_stock_dt, {'Year' : int(vars(self)[field_name]['year'][i])})
-                str = tmp_stock_dt['Type'][i]
-                val = float(re.findall('_', tmp_stock_dt['Type'][i]))
-                tmp_stock_dt['Size'][i] = str[:val - 1]
-                tmp_stock_dt['Technology'][i] = str[:val + 1200]
-                tmp_stock_dt['Type'][i] = None
+                str                             = tmp_stock_dt['Type'][i]
+                val                             = float(re.findall('_', tmp_stock_dt['Type'][i]))
+                tmp_stock_dt['Size'][i]         = str[:val - 1]
+                tmp_stock_dt['Technology'][i]   = str[:val + 1200]
+                tmp_stock_dt['Type'][i]         = None
 
-                out = pd.DataFrame()
+                out                             = pd.DataFrame()
                 out.concat(tmp_stock_dt)
-                return out
+        return out
     '''
     Return all fields into a list of pandas.core.frame.DataFrame
     '''
